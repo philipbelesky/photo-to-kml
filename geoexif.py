3078d15345e7bb0867c27bc61a2d7c7f1ec95b18
Philip Belesky <contact@philipbelesky.com>
2017-07-26 15:40:13 +1000

    Correctly handle multi-false parsing / processing

--- geoexif.py
+++ geoexif.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 import argparse
 
+from exifread import process_file
+
 # Arguments
 description = "Convert geo-tagged photographs with EXIF data to a KML " \
               "PhotoOverlay."
 parser = argparse.ArgumentParser(description=description)
 
-parser.add_argument("filenames", type=argparse.FileType('rb', 0),
-                    help="The file paths of the photos to parse")
+parser.add_argument("filenames", type=argparse.FileType('rb'),
+                    nargs='+', # Gather multiple CLI args into a list
+                    help="The file path of the photo to parse")
 
 args = parser.parse_args()
 
 for file in args.filenames:
-    print(file)
+
+    exif = process_file(file)
+    print(exif)
